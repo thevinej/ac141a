ea7e7a37878d3a3dccb0dd1e59819f07d01fab96
thevinej <laurendev0709@yahoo.com>
2015-12-17 11:10:58 +0800

    Update supplies_inventories.sql

--- supplies_inventories.sql
+++ supplies_inventories.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 14, 2015 at 03:51 AM
+-- Generation Time: Dec 17, 2015 at 04:07 AM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -23,16 +23,23 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `inventories`
+-- Table structure for table `inventory`
 --
 
-CREATE TABLE IF NOT EXISTS `inventories` (
+CREATE TABLE IF NOT EXISTS `inventory` (
   `id` int(11) NOT NULL,
   `inventory_list` varchar(100) NOT NULL,
-  `needs_reorder` tinyint(1) NOT NULL,
+  `needs_reorder` varchar(5) NOT NULL,
   `description` text NOT NULL,
   `supplies_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `inventory`
+--
+
+INSERT INTO `inventory` (`id`, `inventory_list`, `needs_reorder`, `description`, `supplies_id`) VALUES
+(1, 'medicine', 'yes', 'jldhas', 123456);
 
 -- --------------------------------------------------------
 
@@ -59,7 +66,8 @@ CREATE TABLE IF NOT EXISTS `supplies` (
   `item_name` varchar(100) NOT NULL,
   `item_description` text NOT NULL,
   `unit_cost` decimal(11,0) NOT NULL,
-  `re-order_level` int(11) NOT NULL,
+  `onhand` int(11) NOT NULL,
+  `reorder_level` int(11) NOT NULL,
   `other_details` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -68,9 +76,9 @@ CREATE TABLE IF NOT EXISTS `supplies` (
 --
 
 --
--- Indexes for table `inventories`
+-- Indexes for table `inventory`
 --
-ALTER TABLE `inventories`
+ALTER TABLE `inventory`
   ADD PRIMARY KEY (`id`), ADD KEY `supplies_id` (`supplies_id`);
 
 --
@@ -90,10 +98,10 @@ ALTER TABLE `supplies`
 --
 
 --
--- AUTO_INCREMENT for table `inventories`
+-- AUTO_INCREMENT for table `inventory`
 --
-ALTER TABLE `inventories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `inventory`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `inventory_locations`
 --
@@ -107,3 +115,4 @@ ALTER TABLE `supplies`
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
