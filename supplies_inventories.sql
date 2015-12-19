73bb32342ba764ad74571d0f007747066093f189
thevinej <laurendev0709@yahoo.com>
2015-12-19 14:51:40 +0800

    Update supplies_inventories.sql

--- supplies_inventories.sql
+++ supplies_inventories.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
+-- version 4.5.0.2
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 17, 2015 at 04:07 AM
--- Server version: 5.6.24
--- PHP Version: 5.6.8
+-- Generation Time: Dec 19, 2015 at 07:48 AM
+-- Server version: 10.0.17-MariaDB
+-- PHP Version: 5.6.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `supplies_inventories`
@@ -26,20 +26,13 @@ SET time_zone = "+00:00";
 -- Table structure for table `inventory`
 --
 
-CREATE TABLE IF NOT EXISTS `inventory` (
+CREATE TABLE `inventory` (
   `id` int(11) NOT NULL,
   `inventory_list` varchar(100) NOT NULL,
   `needs_reorder` varchar(5) NOT NULL,
   `description` text NOT NULL,
   `supplies_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `inventory`
---
-
-INSERT INTO `inventory` (`id`, `inventory_list`, `needs_reorder`, `description`, `supplies_id`) VALUES
-(1, 'medicine', 'yes', 'jldhas', 123456);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -47,7 +40,7 @@ INSERT INTO `inventory` (`id`, `inventory_list`, `needs_reorder`, `description`,
 -- Table structure for table `inventory_locations`
 --
 
-CREATE TABLE IF NOT EXISTS `inventory_locations` (
+CREATE TABLE `inventory_locations` (
   `id` int(11) NOT NULL,
   `location` text NOT NULL,
   `other_details` text NOT NULL,
@@ -60,16 +53,22 @@ CREATE TABLE IF NOT EXISTS `inventory_locations` (
 -- Table structure for table `supplies`
 --
 
-CREATE TABLE IF NOT EXISTS `supplies` (
+CREATE TABLE `supplies` (
   `id` int(11) NOT NULL,
-  `item_code` int(11) NOT NULL,
   `item_name` varchar(100) NOT NULL,
   `item_description` text NOT NULL,
   `unit_cost` decimal(11,0) NOT NULL,
-  `onhand` int(11) NOT NULL,
+  `on_hand` int(11) NOT NULL,
   `reorder_level` int(11) NOT NULL,
   `other_details` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `supplies`
+--
+
+INSERT INTO `supplies` (`id`, `item_name`, `item_description`, `unit_cost`, `on_hand`, `reorder_level`, `other_details`) VALUES
+(1, 'Medical Gloves', 'These are disposable gloves used during medical examinations and procedures that help prevent contamination between caregivers and patients. ', '530', 10, 5, 'boxes');
 
 --
 -- Indexes for dumped tables
@@ -79,13 +78,15 @@ CREATE TABLE IF NOT EXISTS `supplies` (
 -- Indexes for table `inventory`
 --
 ALTER TABLE `inventory`
-  ADD PRIMARY KEY (`id`), ADD KEY `supplies_id` (`supplies_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `supplies_id` (`supplies_id`);
 
 --
 -- Indexes for table `inventory_locations`
 --
 ALTER TABLE `inventory_locations`
-  ADD PRIMARY KEY (`id`), ADD KEY `inventory_id` (`inventory_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `inventory_id` (`inventory_id`);
 
 --
 -- Indexes for table `supplies`
@@ -101,7 +102,7 @@ ALTER TABLE `supplies`
 -- AUTO_INCREMENT for table `inventory`
 --
 ALTER TABLE `inventory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `inventory_locations`
 --
@@ -111,7 +112,23 @@ ALTER TABLE `inventory_locations`
 -- AUTO_INCREMENT for table `supplies`
 --
 ALTER TABLE `supplies`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD CONSTRAINT `inventory_ibfk_1` FOREIGN KEY (`supplies_id`) REFERENCES `supplies` (`id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `inventory_locations`
+--
+ALTER TABLE `inventory_locations`
+  ADD CONSTRAINT `inventory_locations_ibfk_1` FOREIGN KEY (`inventory_id`) REFERENCES `inventory` (`id`) ON UPDATE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
